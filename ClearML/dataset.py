f41e42e957ded9257c68388055c5847d7bb83d42
Philippe Augusto <66576938+Philippe-Augusto@users.noreply.github.com>
2025-05-20 18:14:39 -0300

    Update dataset.py

--- ClearML/dataset.py
+++ ClearML/dataset.py
@@ -1,23 +1,19 @@
 from clearml import Dataset
 
-# Criar o dataset
 dataset = Dataset.create(
     dataset_name="meu_dataset",
     dataset_project="meu_projeto",
     description="Dataset com arquivos do MinIO local",
-    output_uri="s3://localhost:9000/clearml"  # Ou "s3://localhost:9000/clearml-data" se rodar no host
+    output_uri="s3://localhost:9000/clearml"
 )
 
-# Adicionar arquivos externos do bucket MinIO
-(
-    source_url="s3://localhost:9000/clearml",  # Caminho no bucket MinIO
-    wildcard="*.png",  # Opcional: apenas arquivos JPG
-    #dataset_path="/imagens",  # Caminho relativo no dataset
-    recursive=True,  # Incluir subdiretórios
-    verbose=True  # Mostrar logs
+dataset.add_external_files(
+    source_url="s3://localhost:9000/clearml/",
+    wildcard="*.png",
+    recursive=True,
+    verbose=True
 )
 
-# Sincronizar e finalizar
-dataset.upload()  # Envia metadados ao ClearML
-dataset.finalize(verbose=True, auto_upload=True)  # Fecha o dataset
+dataset.upload()
+dataset.finalize(verbose=True, auto_upload=True)
 print(f"Dataset criado com ID: {dataset.id}")
